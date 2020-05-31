6d5a2954aeb5d0c378e33891b8254beb6abb796d
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-05-31 10:38:54 -0400

    added getFieldName function and cleaned some things up

--- script.js
+++ script.js
@@ -23,35 +23,27 @@ function isValidEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+// Check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(input => {
+        //console.log(input.value);
+        if (input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }    
+    });
+}
+
+//
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 //Event Listeners
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    if(username.value === ''){
-        showError(username, 'Username is required');
-    }else {
-        showSuccess(username);
-    }   
-
-    if(email.value === ''){
-        showError(email, 'Email is required');
-    }else if (!isValidEmail(email.value)){
-        showError(email, 'Email is not valid');
-    }
-    else {
-        showSuccess(email);
-    }   
-
-    if(password.value === ''){
-        showError(password, 'Password is required');
-    }else {
-        showSuccess(password);
-    }   
-
-    if(password2.value === ''){
-        showError(password2, 'Password 2 is required');
-    }else {
-        showSuccess(password2);
-    }   
- 
+    checkRequired([username, email, password, password2]);
 });
